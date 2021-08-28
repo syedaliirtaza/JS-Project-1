0eedb4b2e1c70d6e9ae44916630aacf1f43d209f
syedaliirtaza <aliirtaza401@gmail.com>
2021-08-28 12:05:21 -0700

    style and structure done

---
+++ script.js
@@ -0,0 +1,9 @@
+let buttonNumber = document.querySelectorAll(".btn-number");
+let buttonPlus = document.querySelector(".btn-plus");
+let buttonMinus = document.querySelector(".btn-minus");
+let buttonMultiply = document.querySelector(".btn-multiply");
+let buttonDivide = document.querySelector(".btn-divide");
+let buttonDot = document.querySelector(".btn-dot");
+let buttonEqual = document.querySelector(".btn-equal");
+
+
